9793606d440af4de1e9405aa4c24e27e9db52a2d
Swe <swedhasri08@gmail.com>
2025-10-28 13:49:04 +0530

    Add files via upload

--- Python CSA0818.py
+++ Python CSA0818.py
@@ -1,25 +1,18 @@
-# to find square and cube of a number
-a = eval(input("enter a number : "))
-square = a*a
-cube = a*a*a
-print("Square and Cube of number are " , square , cube)
+# to calculate square of a number
+def sq(n):
+    sqr = n**2
+    return sqr
+n = int(input("enter any integer : "))
+a = sq(n)
+print(a)
 
-# program to find area of circle
-r = eval(input("enter value of radius: "))
-Ar = 3.14*r**2
-print("Area of circle is " , Ar)
-
-# program to find Simple Interest value
-p = eval(input("enter the principle value: "))
-r = eval(input("enter the rate of interest: "))
-t = eval(input("enter the time of period: "))
-SI = (p*r*t)/100
-print(" The value of simple interest " , SI)
-
-#to swap two numbers
-a = int(input("enter any integer : "))
-b = int(input("enter another integer : "))
-a,b=b,a
-print( " here are the swapped numbers " ,a,b)
-
-
+# to find maximum number between two
+def max(n1 , n2):
+    if n1>n2:
+        return n1
+    elif n1<n2:
+        return n2
+    else:
+        return("Both are equal")
+a=max(29 ,49)
+print(a)
